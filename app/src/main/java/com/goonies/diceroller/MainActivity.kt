6594a3defa91acde21b34200808074bf4ce01d2c
mrGoonies <gooniesdev@gmail.com>
2022-07-03 16:16:26 -0400

    Finished project

---
+++ app/src/main/java/com/goonies/diceroller/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.goonies.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.*
+import org.w3c.dom.Text
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Accediendo a las vistas
+        val rollButton: Button = findViewById(R.id.roll_button)
+
+        // rollButton.text = "Let's Roll"
+        rollButton.setOnClickListener {
+            // Mostramos un mensaje en la pantalla utilizando "Toast"
+            //Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
+        }
+
+
+    }
+
+    private fun rollDice() {
+        val randomInt = Random().nextInt(6) + 1
+        val dice_image: ImageView = findViewById(R.id.roll_image)
+        val diceNumber = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // Asignando una imágen a nuestra ImageView
+        dice_image.setImageResource(diceNumber)
+
+    }
+}
